9f5a0b13d363e75445e819a4aa407163e0253ba6
Ansh Mittal <142869477+NINJAHATTORI004@users.noreply.github.com>
2024-06-21 04:00:14 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,11 @@
+// utils/constants.js
+
+exports.RECLAIM_PROVIDER_ID = {
+  newIntegration: 'NEW_INTEGRATION_SERVICE',
+  gamingAchievements: 'GAMING_ACHIEVEMENTS_SERVICE',  // Add this line
+};
+
+exports.RECLAIM_APP_ID = {
+  NEW_INTEGRATION_SERVICE: 'your-new-integration-app-id',
+  GAMING_ACHIEVEMENTS_SERVICE: 'your-gaming-achievements-app-id',  // Add this line
+};
